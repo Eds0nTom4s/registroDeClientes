b8e7658596a52e2e331756665273b0dda7e18493
Edson Tomas <edsonhacker13@gmail.com>
2025-01-28 19:44:09 +0100

    CORS configuração final

--- src/main/java/com/gestaoDeCliente/app/controllers/AtividadeController.java
+++ src/main/java/com/gestaoDeCliente/app/controllers/AtividadeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import com.gestaoDeCliente.app.services.AtividadeService;
 
 @RestController
 @RequestMapping("/atividades")
+@CrossOrigin(origins = "http://localhost:3000") 
 public class AtividadeController {
 	
 	@Autowired
