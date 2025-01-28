0b45d15bf3fa0cac06eb9fc4689d1262311128a3
Edson Tomas <edsonhacker13@gmail.com>
2025-01-28 13:41:45 +0100

    CORS configuracao 2

--- src/main/java/com/gestaoDeCliente/app/WebConfig.java
+++ src/main/java/com/gestaoDeCliente/app/WebConfig.java
@@ -1,17 +1,10 @@
 package com.gestaoDeCliente.app;
 
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-public class WebConfig implements WebMvcConfigurer {
-	
-	 @Override
-	    public void addCorsMappings(CorsRegistry registry) {
-	        registry.addMapping("/**") // Permite CORS para todas as rotas da API
-	                .allowedOrigins("http://localhost:3000") // Permite requisições do localhost:3000
-	                .allowedMethods("GET", "POST", "PUT", "DELETE") // Permite os métodos necessários
-	                .allowedHeaders("*") // Permite todos os cabeçalhos
-	                .allowCredentials(true); // Permite enviar cookies e autenticação, se necessário
-	    }
+@Configuration
+@CrossOrigin(origins = "http://localhost:3000")  // Configura CORS globalmente
+public class WebConfig{
 	
 }
